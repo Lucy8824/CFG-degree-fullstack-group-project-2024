f3360dc17731ed1e193a6c434ed7b4db635fd6fe
lcduval88 <lcduval88@gmail.com>
2024-08-08 14:37:34 +0200

    installed React router and added router component to app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,20 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"; 
 import LoginPage from "./components/LoginPage.jsx";
 import CustomButton from "./components/CustomButton.jsx"; 
 
 
 const App = () => {
   return (
+    <Router>
 <div id="Root">
     <div className="App">
       <LoginPage />
     <CustomButton forgotPassword="forgotPassword" buttonText="Forgotten your password?" />
    <CustomButton signUp="Sign up" buttonText="Don't have an account? Sign up"/>
     </div>
-</div>  
+</div> 
+</Router> 
 );
 }
 
