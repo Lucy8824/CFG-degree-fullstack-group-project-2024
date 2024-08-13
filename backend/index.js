49a83c33da3cd426bd893366495174c71c882566
lcduval88 <lcduval88@gmail.com>
2024-08-13 16:21:31 +0200

    changed syntax of token header in .env file to authorization, added split to token const in index.js and tested in Postman. Seems to validate and successfully verify

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const jwt = require(`jsonwebtoken`);
 const port = process.env.PORT || 3003;
 
 const app = express();
-
+app.use(express.json());
 app.use(cors());
 
 app.get(`/`, (req, res) => {
@@ -22,37 +22,39 @@ app.post(`/user/generateToken`, (req, res) => {
   // this validates user
   const { username, password } = req.body;
   if (username === "validUser" && password === "validPassword") {
+    // this generate JWT token if validation succeded
+    let jwtSecretKey = process.env.JWT_SECRET_KEY;
+    let data = {
+      time: Date(),
+      userID: username, // stored user identifier in jwt payload
+    };
+
+    //sign jwt with secret key
+    const token = jwt.sign(data, jwtSecretKey, { expiresIn: "1h" });
+
+    //send token in response
+    res.send(token);
+  } else {
+    res.status(401).send("Invalid credentials");
   }
-  // this generate JWT token if validation succeded
-  let jwtSecretKey = process.env.JWT_SECRET_KEY;
-  let data = {
-    time: Date(),
-    userID: username, // stored user identifier in jwt payload
-  };
-
-  //sign jwt with secret key
-  const token = jwt.sign(data, jwtSecretKey, { expiresIn: "1h" });
-
-  //send token in response
-  res.send(token);
 });
 
 // now verify JWT with get request
 
 app.get("/user/validateToken", (req, res) => {
   //tokens are passed in header of request for security
-  let = tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
-  let = jwtSecretKey = process.env.JWT_SECRET_KEY;
+  let tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
+  let jwtSecretKey = process.env.JWT_SECRET_KEY;
   try {
-    const token = req.header(tokenHeaderKey);
+    const token = req.header(tokenHeaderKey)?.split(" ")[1];
     const verified = jwt.verify(token, jwtSecretKey);
     if (verified) {
-      return res.send("successfully verified");
+      return res.send("Successfully verified");
     } else {
-      return res.status(401).send(error);
+      return res.status(401).send("Verification failed");
     }
   } catch (error) {
-    return res.status(401).send(error);
+    return res.status(401).send("Invalid token");
   }
 });
 
