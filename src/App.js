6cd5a6b88b3719fc8285cc0bd00be9ea22741aa2
ViktoriiaMorozenko <113129727+ViktoriiaMorozenko@users.noreply.github.com>
2024-08-14 16:13:33 +0100

    Created modal for resert password

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import React from 'react';
 import './App.css';
 import NavBar from './NavBar';
 import ModalForgotPassword from './ModalForgotPassword';
+import ModalResetPassword from './ModalResetPassword'
 
 export default function App() {
   return (
     <div className="App">
       <NavBar />
       <ModalForgotPassword />
+      
+      <ModalResetPassword />
     </div> 
     
   );
