e09869c04db6bc785e34ad44aee13ca81eba0a4f
lcduval88 <lcduval88@gmail.com>
2024-08-14 15:02:46 +0200

    added bcrypt package to has password, created endpoint to register users, created sql table but currently endpoint not working

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ console.log(process.env);
 
 const express = require(`express`);
 const cors = require(`cors`);
+const pool = require(`./pool.js`);
+const bcrypt = require(`bcrypt`);
 const jwt = require(`jsonwebtoken`);
 
 const port = process.env.PORT || 3003;
@@ -17,46 +19,71 @@ app.get(`/`, (req, res) => {
   });
 });
 
-//request to generate JWT with post
-app.post(`/user/generateToken`, (req, res) => {
-  // this validates user
-  const { username, password } = req.body;
-  if (username === "validUser" && password === "validPassword") {
-    // this generate JWT token if validation succeded
-    let jwtSecretKey = process.env.JWT_SECRET_KEY;
-    let data = {
-      time: Date(),
-      userID: username, // stored user identifier in jwt payload
-    };
-
-    //sign jwt with secret key
-    const token = jwt.sign(data, jwtSecretKey, { expiresIn: "1h" });
-
-    //send token in response
-    res.send(token);
-  } else {
-    res.status(401).send("Invalid credentials");
-  }
-});
-
-// now verify JWT with get request
-
-app.get("/user/validateToken", (req, res) => {
-  //tokens are passed in header of request for security
-  let tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
-  let jwtSecretKey = process.env.JWT_SECRET_KEY;
+// endpoint to register new users
+app.post(`/register`, async (req, res) => {
+  const { email, password } = req.body;
   try {
-    const token = req.header(tokenHeaderKey)?.split(" ")[1];
-    const verified = jwt.verify(token, jwtSecretKey);
-    if (verified) {
-      return res.send("Successfully verified");
-    } else {
-      return res.status(401).send("Verification failed");
+    // checks if email already exists
+    const [rows] = await pool.query("SELECT * FROM users WHERE email = ?", [
+      email,
+    ]);
+    if (rows.length > 0) {
+      return res.status(400).json({ message: "Email already registered" });
     }
+    // hash password before saving with bcrypt
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // insert new user into sql query
+    const insertUserQuery = "INSERT INTO USERS (email, password) VALUES (?, ?)";
+    await pool.query(insertUserQuery, [email, hashedPassword]);
+
+    res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
-    return res.status(401).send("Invalid token");
+    console.error("Error registering user:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
 });
+
+// //request to generate JWT with post
+// app.post(`/user/generateToken`, (req, res) => {
+//   // this validates user
+//   const { username, password } = req.body;
+//   if (username === "validUser" && password === "validPassword") {
+//     // this generate JWT token if validation succeded
+//     let jwtSecretKey = process.env.JWT_SECRET_KEY;
+//     let data = {
+//       time: Date(),
+//       userID: username, // stored user identifier in jwt payload
+//     };
+
+//     //sign jwt with secret key
+//     const token = jwt.sign(data, jwtSecretKey, { expiresIn: "1h" });
+
+//     //send token in response
+//     res.send(token);
+//   } else {
+//     res.status(401).send("Invalid credentials");
+//   }
+// });
+
+// // now verify JWT with get request
+
+// app.get("/user/validateToken", (req, res) => {
+//   //tokens are passed in header of request for security
+//   let tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
+//   let jwtSecretKey = process.env.JWT_SECRET_KEY;
+//   try {
+//     const token = req.header(tokenHeaderKey)?.split(" ")[1];
+//     const verified = jwt.verify(token, jwtSecretKey);
+//     if (verified) {
+//       return res.send("Successfully verified");
+//     } else {
+//       return res.status(401).send("Verification failed");
+//     }
+//   } catch (error) {
+//     return res.status(401).send("Invalid token");
+//   }
+// });
 
 app.listen(port, () => {
   console.log(`listening on port ${port}.`);
---
+++ backend/login-users.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE login_users;
+
+USE login_users;
+
+CREATE TABLE
+    IF NOT EXISTS users (
+        id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+        email VARCHAR(250) NOT NULL UNIQUE,
+        password VARCHAR(250) NOT NULL,
+    );
+
+-- commented this out and used directly in index.js to make it easier
+-- INSERT INTO
+--     users (email, name)
+-- VALUES
+--     (?, ?);
