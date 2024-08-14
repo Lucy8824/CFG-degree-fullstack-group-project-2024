4d4e4d1000edf08a802bda1b0d79c9c4e838ccdc
lcduval88 <lcduval88@gmail.com>
2024-08-14 15:51:18 +0200

    changed variables in .env file, changed ports and created sql table in myql and tested in Postman successfully

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const pool = require(`./pool.js`);
 const bcrypt = require(`bcrypt`);
 const jwt = require(`jsonwebtoken`);
 
-const port = process.env.PORT || 3003;
+const port = process.env.PORT || 3006;
 
 const app = express();
 app.use(express.json());
@@ -45,45 +45,44 @@ app.post(`/register`, async (req, res) => {
 });
 
 // //request to generate JWT with post
-// app.post(`/user/generateToken`, (req, res) => {
-//   // this validates user
-//   const { username, password } = req.body;
-//   if (username === "validUser" && password === "validPassword") {
-//     // this generate JWT token if validation succeded
-//     let jwtSecretKey = process.env.JWT_SECRET_KEY;
-//     let data = {
-//       time: Date(),
-//       userID: username, // stored user identifier in jwt payload
-//     };
+app.post(`/user/generateToken`, (req, res) => {
+  // this validates user
+  const { username, password } = req.body;
+  if (username === "validUser" && password === "validPassword") {
+    // this generate JWT token if validation succeded
+    let jwtSecretKey = process.env.JWT_SECRET_KEY;
+    let data = {
+      time: Date(),
+      userID: username, // stored user identifier in jwt payload
+    };
 
-//     //sign jwt with secret key
-//     const token = jwt.sign(data, jwtSecretKey, { expiresIn: "1h" });
+    //sign jwt with secret key
+    const token = jwt.sign(data, jwtSecretKey, { expiresIn: "1h" });
 
-//     //send token in response
-//     res.send(token);
-//   } else {
-//     res.status(401).send("Invalid credentials");
-//   }
-// });
+    //send token in response
+    res.send(token);
+  } else {
+    res.status(401).send("Invalid credentials");
+  }
+});
 
-// // now verify JWT with get request
-
-// app.get("/user/validateToken", (req, res) => {
-//   //tokens are passed in header of request for security
-//   let tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
-//   let jwtSecretKey = process.env.JWT_SECRET_KEY;
-//   try {
-//     const token = req.header(tokenHeaderKey)?.split(" ")[1];
-//     const verified = jwt.verify(token, jwtSecretKey);
-//     if (verified) {
-//       return res.send("Successfully verified");
-//     } else {
-//       return res.status(401).send("Verification failed");
-//     }
-//   } catch (error) {
-//     return res.status(401).send("Invalid token");
-//   }
-// });
+// now verify JWT with get request
+app.get("/user/validateToken", (req, res) => {
+  //tokens are passed in header of request for security
+  let tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
+  let jwtSecretKey = process.env.JWT_SECRET_KEY;
+  try {
+    const token = req.header(tokenHeaderKey)?.split(" ")[1];
+    const verified = jwt.verify(token, jwtSecretKey);
+    if (verified) {
+      return res.send("Successfully verified");
+    } else {
+      return res.status(401).send("Verification failed");
+    }
+  } catch (error) {
+    return res.status(401).send("Invalid token");
+  }
+});
 
 app.listen(port, () => {
   console.log(`listening on port ${port}.`);
--- backend/login-users.sql
+++ backend/login-users.sql
@@ -3,10 +3,10 @@ CREATE DATABASE login_users;
 USE login_users;
 
 CREATE TABLE
-    IF NOT EXISTS users (
+    users (
         id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
         email VARCHAR(250) NOT NULL UNIQUE,
-        password VARCHAR(250) NOT NULL,
+        password VARCHAR(250) NOT NULL
     );
 
 -- commented this out and used directly in index.js to make it easier
