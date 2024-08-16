d0c3233b52ab762690c6123ef5aa12b842717ebb
Esme <esgarrard95@gmail.com>
2024-08-16 10:41:00 +0100

    added to the endpoints (still untested)

--- backend/userApis.js
+++ backend/userApis.js
@@ -1,8 +1,15 @@
 const pool = require(`./pool`);
 
 /// update userProfile endpoint
-app.put('/updateProfile/:id' (req, res) => {
-    const user_id = req.params.id; 
+app.put('/updateProfile/:id', authenticateToken, (req, res) => {
+    const profileID = req.params.id; //the profile ID being edited
+    const userid = req.params.id; //the logged-in user ID extracted from the JWT
+
+// this will ensure the logged-in user is only editing their own profile
+if (userID !== profileID) {
+    return res.status(403).send("You are not able to edit this profile")
+}
+
     const {first_name, age, location, about_me, favourite_artists, attended_festivals, plan_to_visit} = req.body;
 
 const updateQuery = `
@@ -14,12 +21,12 @@ location = ?,
 about_me = ?,
 favourite_artists = ?,
 plan_to_visit = ?,
-attende_festivals = ?
+attended_festivals = ?
 WHERE user_id =?`
 
 pool.query(
     updateQuery,
-    [first_name, age, location, about_me, JSON.stringify(favourite_artists), JSON.stringify(festivals_want), JSON.stringify(festivals_attended), user_Id],
+    [first_name, age, location, about_me, JSON.stringify(favourite_artists), JSON.stringify(plan_to_visit), JSON.stringify(attended_festivals), user_Id],
  (err, result) => {
     if (err) {
         console.log(err);
