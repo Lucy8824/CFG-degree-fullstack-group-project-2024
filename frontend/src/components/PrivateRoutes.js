18f4caa56ab7fbe2627bdc0dd8a20f588f23b7ef
lcduval88 <lcduval88@gmail.com>
2024-08-16 12:15:43 +0200

    created PrivateRoutes component

---
+++ frontend/src/components/PrivateRoutes.js
@@ -0,0 +1,10 @@
+import { Outlet, Navigate } from "react-router-dom";
+import { useAuth } from "./AuthProvider";
+
+const PrivateRoutes = () => {
+  const { isAuthenticated } = useAuth();
+
+  return isAuthenticated ? <Outlet /> : <Navigate to="/LoginPage" />;
+};
+
+export default PrivateRoutes;
