e629fe87da9c825c972a46c604b1c01a7fd3a853
Lydia <lhague24@gmail.com>
2024-08-23 12:06:21 +0100

    route from sign up to login page

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -41,7 +41,7 @@ const SignUp = () => {
       console.log("Registration successful", data);
 
       // Navigate to profile or login page on success
-      navigate("/Profile/:user_id");
+      navigate("/");
     } catch (error) {
       console.error("Error during registration:", error);
       setError(error.message);
